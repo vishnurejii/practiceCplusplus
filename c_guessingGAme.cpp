40879b1edfd5e91cebdb541ee083e7a5c6e4c270
vishnurejii <vishnurejii2003@gmail.com>
2026-02-08 23:02:37 +0530

    new commit

---
+++ c_guessingGAme.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<ctime>
+
+int main(){
+
+    srand(time(NULL));
+    int num=(rand()%100)+1;
+
+    int guess,tries;
+
+    do
+    {
+        std::cout<<"enter a num between (1-100 :)\n";
+        std::cin>>guess;
+
+        if(guess > num){
+            std::cout<<"too high!";
+        }else if (guess < num)
+
+        {
+            std::cout<<"too low!";
+        }else{
+            std::cout<<"CORRECT # of tries "<<tries;
+        }
+        tries++;
+        
+    } while (guess != num);
+
+    return 0;
+    
+}
