5dafc96f07c4369fc3faaeb325f27fd42829c9e7
vishnurejii <vishnurejii2003@gmail.com>
2026-02-09 21:09:40 +0530

    updated

--- d_return_keyword.cpp
+++ d_return_keyword.cpp
@@ -2,15 +2,21 @@
 
 
 double square(double length);
+double cube(double length);
 int main(){
 
     double length=5;
     double area=square(length);
+    double cuberesult=cube(length);
 
-    std::cout<<"the are is :"<<area<<"cm^2";
+    std::cout<<"the are is :"<<area<<"cm^2\n";
+    std::cout<<"the cube result is :"<<cuberesult;
 
     return 0;
 }
 double square(double length){
     return length*length;
+}
+double cube(double length){
+    return length*length*length;
 }
