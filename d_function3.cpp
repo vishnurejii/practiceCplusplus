ddec647cac07d60e500e91c6389003f488d5fbe5
vishnurejii <vishnurejii2003@gmail.com>
2026-02-10 19:40:36 +0530

    new commit

---
+++ d_function3.cpp
@@ -0,0 +1,41 @@
+// #include<iostream>
+
+// void printNum(int num);
+// int main(){
+
+//     int num=1;
+//     printNum(num);
+
+// }
+// void printNum(int num){
+//     std::cout<<num;
+// }
+
+// #include<iostream>
+
+// void printNum();
+// int main(){
+
+//     int num=1;
+//     printNum();
+
+// }
+// void printNum(){
+//     int num=5;
+//     std::cout<<num;
+// }
+
+
+#include<iostream>
+
+int num=10;
+void printNum();
+int main(){
+
+    int num=1;
+    printNum();
+
+}
+void printNum(){
+    std::cout<<num;
+}
